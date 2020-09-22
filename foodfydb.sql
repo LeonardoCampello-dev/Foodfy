f5b89620bd0d44fb1805cf29d2969c2a3a5b824f
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-09-22 10:44:26 -0300

    Creating user table and session in the foodfydb.sql file

--- foodfydb.sql
+++ foodfydb.sql
@@ -1,11 +1,18 @@
--- CREATE AND DROP DATABASE
+-- create and drop database
 DROP DATABASE IF EXISTS foodfydb;
 CREATE DATABASE foodfydb;
 
-CREATE TABLE "files" (
-"id" SERIAL PRIMARY KEY,
-"name" text NOT NULL,
-"path" text NOT NULL
+-- users
+CREATE TABLE "users" (
+    "id" SERIAL PRIMARY KEY,
+    "name" text NOT NULL,
+    "email" text UNIQUE NOT NULL,
+    "password" text NOT NULL,
+    "reset_token" text, 
+    "reset_token_expires" text,
+    "is_admin" boolean DEFAULT false,
+    "created_at" timestamp DEFAULT (now()),
+    "updated_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "chefs" (
@@ -27,13 +34,34 @@ CREATE TABLE "recipes" (
 "updated_at" timestamp DEFAULT (now())
 );
 
+-- files
+
+CREATE TABLE "files" (
+"id" SERIAL PRIMARY KEY,
+"name" text NOT NULL,
+"path" text NOT NULL
+);
+
 CREATE TABLE "recipe_files" (
 "id" SERIAL PRIMARY KEY,
 "recipe_id" integer REFERENCES recipes(id) ON DELETE CASCADE,
 "file_id" integer REFERENCES files(id)
 );
 
--- CREATE PROCEDURE 
+-- connect-pg-simple
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+
+WITH (OIDS=FALSE);
+ALTER TABLE "session" 
+ADD CONSTRAINT "session_pkey" 
+PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+-- create procedure
 
 CREATE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
@@ -43,16 +71,28 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- AUTO updated_at CHEFS 
+-- auto updated_at chefs 
 
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON chefs
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
--- AUTO updated_at RECIPES  
+-- auto updated_at recipes  
 
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON recipes
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- auto updated_at users  
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- adding user_id key
+
+ALTER TABLE recipes
+ADD COLUMN user_id integer NOT NULL REFERENCES users(id)
