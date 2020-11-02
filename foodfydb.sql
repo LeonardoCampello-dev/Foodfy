11470bc9d7efe1606668bc181d424cc17c0dcfd4
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-11-02 20:54:17 -0300

    Adding database cleaning routine

--- foodfydb.sql
+++ foodfydb.sql
@@ -2,50 +2,51 @@
 DROP DATABASE IF EXISTS foodfydb;
 CREATE DATABASE foodfydb;
 
--- users
+-- create tables
+
 CREATE TABLE "users" (
-    "id" SERIAL PRIMARY KEY,
-    "name" text NOT NULL,
-    "email" text UNIQUE NOT NULL,
-    "password" text NOT NULL,
-    "reset_token" text, 
-    "reset_token_expires" text,
-    "is_admin" boolean DEFAULT false,
-    "created_at" timestamp DEFAULT (now()),
-    "updated_at" timestamp DEFAULT (now())
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "reset_token" text, 
+  "reset_token_expires" text,
+  "is_admin" boolean DEFAULT false,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "chefs" (
-"id" SERIAL PRIMARY KEY,
-"file_id" integer NOT NULL REFERENCES "files" (id),
-"name" text NOT NULL,
-"created_at" timestamp DEFAULT (now()),
-"updated_at" timestamp DEFAULT (now())
+  "id" SERIAL PRIMARY KEY,
+  "file_id" integer NOT NULL REFERENCES "files" (id),
+  "name" text NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "recipes" (
-"id" SERIAL PRIMARY KEY,
-"chef_id" integer NOT NULL REFERENCES "chefs"(id),
-"title" text NOT NULL,
-"ingredients" text[] NOT NULL,
-"preparation" text[] NOT NULL,
-"information" text NOT NULL,
-"created_at" timestamp DEFAULT (now()),
-"updated_at" timestamp DEFAULT (now())
+  "id" SERIAL PRIMARY KEY,
+  "chef_id" integer NOT NULL REFERENCES "chefs"(id),
+  "title" text NOT NULL,
+  "ingredients" text[] NOT NULL,
+  "preparation" text[] NOT NULL,
+  "information" text NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
 );
 
 -- files
 
 CREATE TABLE "files" (
-"id" SERIAL PRIMARY KEY,
-"name" text NOT NULL,
-"path" text NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "path" text NOT NULL
 );
 
 CREATE TABLE "recipe_files" (
-"id" SERIAL PRIMARY KEY,
-"recipe_id" integer REFERENCES recipes(id) ON DELETE CASCADE,
-"file_id" integer REFERENCES files(id)
+  "id" SERIAL PRIMARY KEY,
+  "recipe_id" integer REFERENCES recipes(id) ON DELETE CASCADE,
+  "file_id" integer REFERENCES files(id)
 );
 
 -- connect-pg-simple
@@ -71,21 +72,17 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- auto updated_at chefs 
+-- auto updated_at 
 
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON chefs
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
--- auto updated_at recipes  
-
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON recipes
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
-
--- auto updated_at users  
 
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON users
@@ -96,3 +93,17 @@ EXECUTE PROCEDURE trigger_set_timestamp();
 
 ALTER TABLE recipes
 ADD COLUMN user_id integer NOT NULL REFERENCES users(id)
+
+-- database cleanup routine
+DELETE FROM users;
+DELETE FROM chefs;
+DELETE FROM recipes;
+DELETE FROM files;
+DELETE FROM recipe_files;
+
+
+ALTER SEQUENCE users_id_seq RESTART WITH 1;
+ALTER SEQUENCE chefs_id_seq RESTART WITH 1;
+ALTER SEQUENCE recipes_id_seq RESTART WITH 1;
+ALTER SEQUENCE files_id_seq RESTART WITH 1;
+ALTER SEQUENCE recipe_files_id_seq RESTART WITH 1;
